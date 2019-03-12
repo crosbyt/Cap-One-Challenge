41606e701727fd30effb9c73d71e8adfc6963ec7
crosbyt <crosbyt@umich.edu>
2019-03-12 16:53:43 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -123,7 +123,14 @@ searchClear = e => {
 	}
 	
 //add to favorite (BONUS FEATURE WILL ADD LATER)
-
+addFav = e => {
+    if(this.state.searchData != ""){
+      var favorites = ls.get("favorites")
+      favorites = JSON.parse(favorites)
+      favorites.push(this.state.searchData.items[this.state.currentPic])
+      ls.set("favorites", JSON.stringify(favorites))
+    }
+  }
 
 componentLoad() {
       var arr = []
@@ -165,10 +172,182 @@ componentLoad() {
    }
 	
 render() {
-	
-	
+	var arr = []
+	is(this.stat.searchData != ""){
+		var pics = this.state.searchData.items.slice(0,50).map(item,index) => {
+			return(
+        <Col span={6}  style={{paddingTop: 10, paddingRight: 15, paddingLeft: 15}}>
+        <Card value = {index} hoverable cover={<img src= {item.links[0].href} searchHistory= {() => this.showModal(index)} height="200" width="200"/>}
+        >
+        <Meta
+          title={item.data[0].title}
+          onClick= {() => this.showModal(index)}
+        />
+        </Card>
+        </Col>
+      )})
+    }
+    else{
+	    var pics = null
+	    }
+	if(this.state.searchData != "" && ls.get("favorites") != JSON.stringify(arr)){
+		var favPhotos = JSON.parse(ls.get("favorites")).reverse().map((item,index) => {
+      return(
+        <Col span={6}  style={{paddingTop: 10, paddingRight: 15, paddingLeft: 15}}>
+        <Card  hoverable cover={<img src= {item.links[0].href} searchHistory= {() => this.showModal(index)} height="200" width="200"/>}
+        >
+        <Meta
+          title={item.data[0].title}
+          onClick= {() => this.showModal(index)}
+        />
+        </Card>
+        </Col>
+      )
+    })
+    }
+    else{
+	    var favPics = null
 	}
-
+	
+	var centers = options.map(option => {
+		return(
+		<Option value={option.value} searchHistory= {() => this.searchCenter(option.value)} >{option.label}</Option>
+		)}
+	)
+	
+	if(this.state.current == "favorite"){
+		return (
+        <div>
+        <div style= {{position: 'fixed', width: '100%', zIndex: 1}}>
+        <Header>
+        <div className="logo" />
+        <Menu
+          theme="light"
+          mode="horizontal"
+          style={{ lineHeight: '64px', textAlign: 'center' }}
+          onClick={this.menuSelect}
+          selectedKeys={[this.state.current]}
+        >
+        <Menu.Item key="app">
+          <Icon type="camera" />Home Page
+        </Menu.Item>
+        <Menu.Item key="favorite">
+          <Icon type="star" />Favorite Images
+        </Menu.Item>
+        </Menu>
+      </Header>
+        </div>
+        <div style = {{paddingTop: '5%'}}>
+        {favPhotos}
+        </div>
+        {(this.state.searchData != "" && ls.get("favorites") != JSON.stringify(arr))
+        ?<Modal
+          title="Modal"
+          visible={this.state.visable}
+          onOk={this.handleYes}
+          onCancel={this.handleNo}
+          title = {JSON.parse(ls.get("favorites"))[this.state.currentFavorite].data[0].title}>
+          <p> Center: {JSON.parse(ls.get("favorites"))[this.state.currentFavorite].data[0].center} </p>
+          <p> Date Created: {JSON.parse(ls.get("favorites"))[this.state.currentFavorite].data[0].date_created}</p>
+          <p> Description: {JSON.parse(ls.get("favorites"))[this.state.currentFavorite].data[0].description_508}</p>
+          <div style= {{textAlign: "center"}}>
+          </div>
+        </Modal>:
+        <div/>
+        }
+        </div>
+      )
+    }
+	var arr = []
+	console.log((this.state.searchData.items))
+	return (
+	<div className="App">
+      <div style= {{position: 'fixed', width: '100%', zIndex: 1}}>
+      <Header style = {{width: '100%'}}>
+      <div className="logo" />
+      <Menu
+        theme="light"
+        mode="horizontal"
+        style={{ lineHeight: '64px' }}
+        menuSelect={this.menuSelect}
+        selectedKey={[this.state.current]}
+      >
+      <Menu.Item key="app">
+        <Icon type="camera" />Home Page
+      </Menu.Item>
+      <Menu.Item key="favorite">
+        <Icon type="star" />Favorite Images
+      </Menu.Item>
+      </Menu>
+      <Search style={{ width: 400, textAlign: 'center'}} placeholder="Search" id = "search" pressEnter={e => this.search(e)} userInput={e => this.userInput(e)} />
+    </Header>
+      </div>
+      <div className= "SearchForm" style = {{paddingTop: '8%'}}>
+      <div className= "MoreOptions" style = {{paddingLeft: "35.4%", paddingTop: "1%"}}>
+      <Collapse  defaultActiveKey={['0']} style={{ width: 400}}>
+        <Panel header="More Search Options" key="1">
+          <Col>
+          <div><strong>Search by Start Year and/or End Year</strong></div>
+          <br/>
+          <Search style={{ width: 350}} type="number"  placeholder="Start Year" id= "startYear" userInput={e => this.userInput(e)}/>
+          <Search style={{ width: 350}} type="number" placeholder="End Year" id= "endYear" userInput={e => this.userInput(e)}/>
+          <div><strong>Search by NASA Centers</strong></div>
+          <br/>
+          <Select defaultValue="All" style={{ width: 300 }} options={options} id = "center">
+          {centers}
+          </Select>
+          </Col>
+        </Panel>
+        <Panel header="Recent Searches" key="3">
+          <Col>
+          </Col>
+          {(ls.get("updatedHistory") != null)
+          ?<List
+            header={<div><strong>Click Item to Search</strong></div>}
+             size="small"
+             dataSource={JSON.parse(ls.get("updatedHistory")).reverse().slice(0,5)}
+             renderItem={item => (
+               <List.Item style = {{cursor: "pointer"}} value = {item}  userInput = {() => this.searchHistory(item)}>{item}</List.Item>
+             )}
+           />:
+           <div></div>
+         }
+          <Button  type="secondary" htmlType="submit" userInput = {e => this.searchClear(e)}> Clear History </Button>
+        </Panel>
+      </Collapse>
+      </div>
+      <br/>
+      <Button  type="primary" htmlType="submit" userInput = {e => this.search(e)}> Submit </Button>
+      </div>
+      {photos}
+      {(this.state.searchData != "" && this.state.searchData != "null")
+      ?<Modal
+        title="Modal"
+        visible={this.state.visible}
+        onOk={this.handleYes}
+        onCancel={this.handleNo}
+        title = {this.state.searchData.items[this.state.currentPic].data[0].title}>
+        <p> Center: {this.state.searchData.items[this.state.currentPic].data[0].center} </p>
+        <p> Date Created: {this.state.searchData.items[this.state.currentPic].data[0].date_created} </p>
+        {(this.state.searchData.items[this.state.currentPic].data[0].photographer != null)
+          ?<p> Photogapher: {this.state.searchData.items[this.state.currentPic].data[0].photographer} </p>
+          :<div></div>
+        }
+        {(this.state.searchData.items[this.state.currentPic].data[0].description_508 != null)
+        ?<p> Description: {this.state.searchData.items[this.state.currentPic].data[0].description_508} </p>
+        :<p>Description: {this.state.searchData.items[this.state.currentPic].data[0].description} </p>
+        }
+        <div style= {{textAlign: "center"}}>
+        <Button icon="star" userInput = {e => this.addFav(e)}>Add to Favorites</Button>
+        </div>
+      </Modal>:
+      <div/>
+      }
+      </div>
+    );
+  }
+	
+}
 
 }
 
